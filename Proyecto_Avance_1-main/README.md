821d8890cbc069894a85ae510d5ea99b3052173b
AleAlpizar <133933772+AleAlpizar@users.noreply.github.com>
2024-03-12 05:55:14 -0600

    Aporte Alexandra Alpizar, historias de usuario
    HU3,Escenario 1 Explorar locales asociados desde la página de inicio, Un usuario nuevo está buscando un lugar para cenar y visita la página web de un restaurante local, El usuario ingresa a la página de inicio del sitio web del restaurante, En la sección de inicio, el usuario ve una foto del local y una descripción que resalta los aspectos más destacados del restaurante. Además, el usuario encuentra un botón   que dice "Ver locales asociados". Al hacer clic en este botón, se abre una lista de otros restaurantes asociados con información básica.          HU3,Escenario 2, Visualización de información del local en la página de inicio, Un usuario está buscando un lugar para celebrar un evento especial y decide explorar opciones en línea, El usuario accede a la página de inicio del sitio web de un local, En la sección de inicio, el usuario encuentra una foto del local del restaurante que muestra el ambiente y el estilo del lugar. En el mismo apartado de la foto, encuentra información clave sobre el local.                      HU5, Escenario 1, Acceso a información de contacto y redes sociales desde la sección "Contáctenos", Un cliente potencial está interesado en obtener más información sobre una empresa local y desea ponerse en contacto con ellos para hacer preguntas o solicitar servicios, El cliente visita el sitio web de la empresa y busca la sección "Contáctenos", El cliente puede elegir el método de contacto que prefiera y conectarse fácilmente con la empresa a través de varios canales.

---
+++ Proyecto_Avance_1-main/README.md
@@ -0,0 +1,2 @@
+# Proyecto_Avance_2
+ Avance 2 Proyecto Villa gastronomica Grupo 6
---
+++ Proyecto_Avance_1-main/src/test/java/Grupo_6/Grupo_6_Proyecto/Grupo6ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo_6.Grupo_6_Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo6ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
