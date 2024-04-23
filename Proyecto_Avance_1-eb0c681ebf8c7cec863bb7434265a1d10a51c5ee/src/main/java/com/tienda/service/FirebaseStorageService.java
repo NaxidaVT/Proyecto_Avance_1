7d34d3f3cde14d9cf67950b3c746da6d229c51e9
Naxida <145503760+NaxidaVT@users.noreply.github.com>
2024-04-23 08:07:32 -0600

    Push Kevin Firebase Hecha y configurada

--- Proyecto_Avance_1-eb0c681ebf8c7cec863bb7434265a1d10a51c5ee/src/main/java/com/tienda/service/FirebaseStorageService.java
+++ Proyecto_Avance_1-eb0c681ebf8c7cec863bb7434265a1d10a51c5ee/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -7,14 +7,14 @@ public interface FirebaseStorageService {
     public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
 
     //El BuketName es el <id_del_proyecto> + ".appspot.com"
-    final String BucketName = "techshop-curso.appspot.com";
+    final String BucketName = "VillaGastronomica-curso.appspot.com";
 
     //Esta es la ruta básica de este proyecto Techshop
-    final String rutaSuperiorStorage = "techshop";
+    final String rutaSuperiorStorage = "VillaGastronomica";
 
     //Ubicación donde se encuentra el archivo de configuración Json
     final String rutaJsonFile = "firebase";
     
     //El nombre del archivo Json
-    final String archivoJsonFile = "techshop-curso-firebase-adminsdk-ik8h4-d999462bf9.json";
+    final String archivoJsonFile = "VillaGastronomica-curso-firebase-adminsdk-ik8h4-d999462bf9.json";
 }
---
+++ Proyecto_Avance_1-eb0c681ebf8c7cec863bb7434265a1d10a51c5ee/src/test/java/Clasessinuso/CorreoService.java
@@ -0,0 +1,14 @@
+package Clasessinuso;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    
+    public void enviarCorreoHtml(
+            
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+    
+}
